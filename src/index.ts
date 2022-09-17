a3181fdfe8f22c0fbf96abe79eeb470f8b765e2f
Renato Santos <renato.santos0611@gmail.com>
2022-09-17 09:45:04 -0300

    PORT change

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ var path = require("path");
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("Listening... 4000");
 });
