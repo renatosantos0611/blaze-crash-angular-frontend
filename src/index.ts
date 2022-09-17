0358a800240881a9a43d35d302fe5d153571c7e8
Renato Santos <renato.santos0611@gmail.com>
2022-09-17 09:49:38 -0300

    added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+require("dotenv").config();
 
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log("Listening... 4000");
+  console.log(`Listening... ${process.env.PORT} or 4000`);
 });
