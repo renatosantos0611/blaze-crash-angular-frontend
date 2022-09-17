80814bd454aefa6bb8e3cc5fd8b2d18cd62dace6
Renato Santos <renato.santos0611@gmail.com>
2022-09-17 09:43:32 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
+
+app.listen(4000, () => {
+  console.log("Listening... 4000");
+});
